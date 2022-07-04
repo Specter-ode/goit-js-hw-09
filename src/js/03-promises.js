263ffff8c44b56bd3ebdd027c45bb1572a8e371a
Vlad Shchelkunov <specter.ode@gmail.com>
2022-07-04 13:35:45 +0300

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,13 +13,38 @@ function createPromise(position, delay) {
     // Reject
   }
 }
-console.log(delayEl.value += stepEl.value)
-console.log(amountEl.value += stepEl)
-const makePromise = () => {
-  return new Promise ((resolve, reject))
-}
 
+const makePromise = new Promise((resolve, reject) => {
+  const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+  if (shouldResolve) {
+    resolve('promise succes')
+  }
+  reject('promise error');
+}, 2000);
+});
+makePromise.then(succes, mistake);
+function succes(result) {
+  console.log(`${result}`)
+}
+function mistake(error) {
+  console.log(`${error}`)
+}
 function onBtnSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
